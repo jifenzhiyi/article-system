17610a9877b0ecd8999bc1b8ff34aaf66a3de5cc
MaluFE <malufe@MaluAdmindeiMac.local>
2020-11-06 10:01:52 +0800

    :bug: config配置更新

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const NODE_ENV = process.env.NODE_ENV || 'dev';
 export const API_LIST = {
   // dev: 'http://localhost:8887/',
   dev: 'http://192.168.188.16:9229/malu/', // 测试地址
-  prod: `${window.location.origin}/malu-superuser/`,
+  prod: `${window.location.origin}/malu/`,
 };
 
 export const END_POINT = API_LIST[NODE_ENV];
